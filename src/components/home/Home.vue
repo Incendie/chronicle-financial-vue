ccf7e00be1d5b576c91214d512eb41d587c2e6a5
Incendie <incendie323@gmail.com>
2018-08-11 13:45:08 -0400

    home page styling

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="hero">
-      <h1>Live Life to the Fullest</h1>
+			<div class="wrapper">
+		<h1>Live Life to the Fullest</h1>
+			</div>
     </div>
     <main class="wrapper">
       <h2>About Us & Our Services</h2>
@@ -14,34 +16,36 @@
       <p>Our investment professionals have the expertise, dedication and experience to make insightful investment decisions, while being committed to the principle that in every action we take, we will be guided by the long-term best interests of our clients.</p>
     </main>
   
-    <section class="about wrapper">
-			<h2>Learn More About Us</h2>
-			<ul class="links">
-				<li>
-					<div class="links__container">
-						<div class="links__img"><img src="../../assets/D88_8728.jpg" alt=""></div>
-						<h2>Meet Our Advisors</h2>
-						<p>Meet our team of qualified, passionate advisors!</p>
-						<button>Read More</button>
-					</div>
-				</li>
-				<li>
-					<div class="links__container">
-						<div class="links__img"><img src="../../assets/wedding-1770860.jpg" alt=""></div>
-						<h2>Did You Know?</h2>
-						<p>In order to retire comfortably, you need 70% of your working income! Are you prepared?</p>
-						<button>Read More</button>
-					</div>
-				</li>
-				<li>
-					<div class="links__container">
-						<div class="links__img"><img src="../../assets/stock-photo-cheerful-business-people-working-with-documents-together-415652407.jpg" alt=""></div>
-						<h2>Free 30Min Consultation</h2>
-						<p>We'd like to provide you with a second opinion. Come and speak to us!</p>
-						<button>Read More</button>
-					</div>
-				</li>
-			</ul>
+    <section class="about">
+			<div class="wrapper">
+				<h2>Learn More About Us</h2>
+				<ul class="links">
+					<li>
+						<div class="links__container">
+							<router-link to="/advisors" tag="div" class="links__img advisors"></router-link>
+							<h3>Meet Our Advisors</h3>
+							<p>Meet our team of qualified, passionate advisors!</p>
+							<router-link to="/advisors" tag="button">Read More</router-link>
+						</div>
+					</li>
+					<li>
+						<div class="links__container">
+							<router-link to="/services" tag="div" class="links__img know"></router-link>
+							<h3>Did You Know?</h3>
+							<p>In order to retire comfortably, you need 70% of your working income! Are you prepared?</p>
+							<router-link to="/services" tag="button">Read More</router-link>
+						</div>
+					</li>
+					<li>
+						<div class="links__container">
+							<router-link to="/contact" tag="div" class="links__img consult"></router-link>
+							<h3>Free 30Min Consultation</h3>
+							<p>We'd like to provide you with a second opinion. Come and speak to us!</p>
+							<router-link to="/contact" tag="button">Read More</router-link>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</section>
   </div>
 </template>
