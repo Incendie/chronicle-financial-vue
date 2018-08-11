89bc58e133d9980099a9986756308dd5bf941490
Incendie <incendie323@gmail.com>
2018-08-11 12:49:58 -0400

    installed font awesome, footer styling

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-    <section class="contact wrapper">
-      <h2>Contact Us</h2>
-      <ul class="contact__info">
-        <li>
-          <img src="" alt="">
-          <p>905 597 7780</p>
-        </li>
-        <li>
-          <img src="" alt="">
-          <p>E-Mail</p>
-        </li>
-      </ul>
-      <div class="contact__address">
-        <p>20 Wertheim Crt. Unit #19</p>
-        <p>Richmond Hill, On</p>
-        <p>L4B 3H7</p>
+    <section class="contact">
+      <div class="wrapper">
+        <h2>Contact Us</h2>
+        <ul class="contact__info">
+          <li>
+            <font-awesome-icon icon="phone" size="2x" rotate="90"/>
+            <p>905 597 7780</p>
+          </li>
+          <li>
+            <font-awesome-icon icon="envelope" size="2x"/>
+            <p>E-Mail</p>
+          </li>
+        </ul>
+        <div class="contact__address">
+          <p>20 Wertheim Crt. Unit #19</p>
+          <p>Richmond Hill, On</p>
+          <p>L4B 3H7</p>
+        </div>
       </div>
     </section>
 
-    <footer class="wrapper">
-      <p>Copyright © ACTS Financial Corp.</p>
-      <p>*Mutual Funds are sold through TeamMax Investment Corp. The information and data contained herein have been obtained from sources believed to be reliable. We do not represent, warrant or guarantee that they are accurate or complete or that they should be relied upon. Nothing contained herein shall be construed as an offer, invitation, advertisement or inducement for purchase or sale of any securities, products or services. We accept no liability for any loss or damage howsoever arising by reliance on any information given or opinion expressed on this Web site. Financial markets inherently have a certain extent of risk and the investment value may go down as well as up.</p>
+    <footer>
+      <div class="wrapper">
+        <p>Copyright © ACTS Financial Corp.</p>
+        <p>*Mutual Funds are sold through TeamMax Investment Corp. The information and data contained herein have been obtained from sources believed to be reliable. We do not represent, warrant or guarantee that they are accurate or complete or that they should be relied upon. Nothing contained herein shall be construed as an offer, invitation, advertisement or inducement for purchase or sale of any securities, products or services. We accept no liability for any loss or damage howsoever arising by reliance on any information given or opinion expressed on this Web site. Financial markets inherently have a certain extent of risk and the investment value may go down as well as up.</p>
+      </div>
     </footer>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue'
-import router from './router';
+import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faPhone, faEnvelope)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
